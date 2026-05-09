cac653108a9fc27cefec23f5b76c17e040227383
KartoffelChips <104089082+KartoffelChipss@users.noreply.github.com>
2026-05-09 10:45:52 +0200

    Move zetacore to root of project & remove testplugin

--- plugin-test/src/main/java/com/zetaplugins/pluginTest/GenericCountService.java
+++
@@ -1,9 +0,0 @@
-package com.zetaplugins.pluginTest;
-
-import java.util.UUID;
-
-public interface GenericCountService {
-    void incrementCounter(UUID playerId);
-
-    int getCounter(UUID playerId);
-}
